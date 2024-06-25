e24365c1768ad6848a1d144694822f6dab142132
Hichandayo <nguyenhienthe1120@gmail.com>
2024-06-25 10:47:35 +0700

    up

--- BaiTapXIII/Bai1/Main.java
+++ BaiTapXIII/Bai1/Main.java
@@ -4,9 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
+         //tạo 1 array để chưa dữ liệu
     public static void main(String[] args) {
         int[] elements = {1, 2, 3, 4, 5,2,3,1,5,2,1};
+        //sử dụng hashMap để lưu trữ tần số xuất hiện của phần tử
     Map<Integer, Integer> countMap = new HashMap<>();
+    //sử dụng vòng lặp for để truy tìm tần suất xuất hiện của phần tử trong mảng
     for(int num : elements) {
         if (countMap.containsKey(num)){// Nếu phần tử đã có trong HashMap, tăng số lần xuất hiện lên 1
             countMap.put(num, countMap.get(num)+1);
@@ -14,6 +17,7 @@ public class Main {
             countMap.put(num, 1);
         }
     }
+    //in ra
         System.out.println("Số lần xuất hiện của từng phần tử:");
     for (int key : countMap.keySet() ){
         System.out.println(key + " xuất hiện " + countMap.get(key) +" lần");
--- BaiTapXIII/Bai3/Main.java
+++ BaiTapXIII/Bai3/Main.java
@@ -1,14 +1,24 @@
 package BaiTapXIII.Bai3;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        HashMap<Integer,Integer>hashMap = new HashMap<>();
+        Map<Integer, Integer> hashMap = new HashMap<>();
+        hashMap.put(2, 5);
         hashMap.put(1,2);
-        List<Map.Entry<String, Integer>> entryList = new ArrayList<>(hashMap.entrySet());
+        hashMap.put(5, 6);
+        hashMap.put(3, 1);
+
+        // Chuyển đổi HashMap thành một danh sách List<Map.Entry<Integer, Integer>>
+        List<Map.Entry<Integer, Integer>> list = new ArrayList<>(hashMap.entrySet());
+
+        // Sắp xếp danh sách list dựa trên giá trị tăng dần
+        list.sort(Comparator.comparing(Map.Entry::getValue));
+        // In danh sách đã sắp xếp
+        for (Map.Entry<Integer, Integer> entry : list) {
+            System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+        }
+
     }
 }
